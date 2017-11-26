e01fc896146943cb64013d048da221addd1668f0
Ivan Kolev <vancyki@abv.bg>
2017-11-26 16:32:32 +0200

    create list view and layout for the category cell

--- app/src/main/java/com/inkole/coderswag/Model/Category.kt
+++ app/src/main/java/com/inkole/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.inkole.coderswag.Model
 /**
  * Created by Vankata on 11/26/2017.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
