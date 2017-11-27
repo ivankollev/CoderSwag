a3edb9f7ee41924e122e06fcfbf8335d43cd5f01
Ivan.Kolev <Ivan.Kolev@ext.gfk.com>
2017-11-27 14:54:37 +0200

    add recycler view

--- app/src/main/java/com/inkole/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/inkole/coderswag/Controller/MainActivity.kt
@@ -2,8 +2,10 @@ package com.inkole.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import com.inkole.coderswag.Adapters.CategoryAdapter
+import com.inkole.coderswag.Adapters.CategoryRecycleAdapter
 import com.inkole.coderswag.Model.Category
 import com.inkole.coderswag.R
 import com.inkole.coderswag.Services.DataService
@@ -11,13 +13,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
